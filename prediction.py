cfe8e63198903e5f42d719f232ec8ae664956f88
abdessamad-ca <abdessamadzarra@gmail.com>
2020-09-26 15:41:49 +0200

    3

--- prediction.py
+++ prediction.py
@@ -1,5 +1,5 @@
 import requests
-BASE_URL = "https://linearregression.azurewebsites.net/"
+BASE_URL = "http://127.0.0.1:5000"
 years_exp = {"YearsExperience": 8}
 response = requests.post("{}/predict".format(BASE_URL), json = years_exp)
 
