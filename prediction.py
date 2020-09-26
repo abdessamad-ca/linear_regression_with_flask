b8898e3c51b8fd3a055a706619e2104d9decdc38
Abdessamad zarra <55798370+abdessamad-ca@users.noreply.github.com>
2020-09-26 15:33:10 +0200

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -1,5 +1,5 @@
 import requests
-BASE_URL = "https://linearregression.azurewebsites.net/"
+BASE_URL = "https://linearregression.azurewebsites.net/predicted"
 years_exp = {"YearsExperience": 8}
 response = requests.post("{}/predict".format(BASE_URL), json = years_exp)
 
